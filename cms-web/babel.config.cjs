ade35a45d172a4acdb2a62272f7e3b4d4b314f47
yohan Moon <munyohan@naver.com>
2021-04-20 20:12:08 +0900

    Rename babel.config.js to babel.config.cjs
    It can make jest work correctly. So I changed it.

--- cms-web/babel.config.cjs
+++ cms-web/babel.config.cjs
@@ -1,3 +1,11 @@
-module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+module.exports = (api) => {
+  api.cache(false);
+  const prod = process.env.MODE_ENV === "production";
+  const presets = ["next/babel", ['@babel/preset-env', { targets: { node: 'current' } }]];
+  const plugins = prod ? ["transform-remove-console"] : [];
+
+  return {
+    presets,
+    plugins,
+  };
 };
