c117f5c8c0620dccd5afe0f8b8648ba070fcc64f
yohan Moon <munyohan@naver.com>
2021-04-21 10:26:53 +0900

    Make some test for user api

--- cms-web/test/mocks/userService.js
+++ cms-web/test/mocks/userService.js
@@ -1,21 +1,51 @@
-export function getAUser(uid) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      const user = {
-        uid: uid,
-        nickname: `User_${uid}`,
-        age: parseInt(Math.random() * 120) + 1,
-      };
+import axios from "axios";
 
-      resolve(user);
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com";
+
+export function getUser(uid) {
+  return axios.get(`${API_ENDPOINT}/users/${uid}`).then(res => res.data);
+
+  // return new Promise(resolve => {
+  //   setTimeout(() => {
+  //     const user = {
+  //       uid,
+  //       nickname: `User_${uid}`,
+  //       age: Math.floor(Math.random() * 120) + 1,
+  //     };
+
+  //     resolve(user);
+  //   }, 100);
+  // });
+}
+
+export function createNewUser(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`New user [${user.nickname}] is successfully created.`);
+
+      if (user.uid < 0) reject(new Error("Fail creating a new user !"));
     }, 100);
   });
 }
 
-export function createNewUser(user) {
-  return new Promise(resolve => {
+export function updateUser(user) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`New user [${user.nickname}] is successfully created.`)
-    }, 100)
-  })
+      if (!user) {
+        reject(new Error("Update failed."));
+      }
+
+      resolve(`User-${user.uid} has been updated.`);
+    }, 100);
+  });
+}
+
+export function deleteUser(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!user) reject(new Error("Delete failed."));
+
+      resolve(`User-${user.uid} has been deleted.`);
+    }, 100);
+  });
 }
--- cms-web/test/users.test.js
+++ cms-web/test/users.test.js
@@ -1,29 +1,74 @@
-import { getAUser, createNewUser } from './mocks/userService';
+import axios from "axios";
+import {
+  getUser,
+  createNewUser,
+  updateUser,
+  deleteUser,
+} from "./mocks/userService";
 
-jest.mock('./mocks/userService');
+jest.mock("./mocks/userService");
+jest.mock("axios");
 
-const user = {
+const userMario = {
   uid: 1,
-    nickname: 'Mario',
-    age: 29
-}
+  nickname: "Mario",
+  age: 29,
+};
 
 beforeEach(() => {
-  user = {
-    uid: 1,
-    nickname: 'Mario',
-    age: 29
-  }
-})
+  getUser.mockClear();
+  createNewUser.mockClear();
+});
 
-test('get a user test', () => {
-  return getAUser().then(user => {
-    expect(user).toHaveProperty('uid');
-    expect(user).toHaveProperty('nickname');
-    expect(user).toHaveProperty('age');
+test("get a user test", async () => {
+  axios.get.mockResolvedValue({
+    data: {
+      uid: 1,
+      nickname: "Mario",
+      age: 30,
+    },
+  });
+
+  // getUser.mockResolvedValue({
+  //   uid: 1,
+  //   nickname: "Mario",
+  //   age: 30,
+
+  const user = await getUser(1);
+
+  expect(user).toHaveProperty("uid", 1);
+  expect(user).toHaveProperty("nickname", "Mario");
+  expect(user).toHaveProperty("age", 30);
+  expect(axios.get).toBeCalledTimes(1);
+  expect(axios.get).toBeCalledWith("https://testapi.url.com/users/1");
+
+  // return getUser().then(user => {
+  //   expect(user).toHaveProperty("uid");
+  //   expect(user).toHaveProperty("nickname");
+  //   expect(user).toHaveProperty("age");
+  // });
+});
+
+test("create a user test", () => {
+  createNewUser.mockResolvedValue("New user Mario is successfully created.");
+
+  return createNewUser(userMario).then(res => {
+    expect(res).toBe("New user Mario is successfully created.");
   });
 });
 
-test('create a user test', () => {
-  return createNewUser(user)
-})
+test("update a user", () => {
+  updateUser.mockResolvedValue(`User-${userMario.uid} has been updated.`);
+
+  return updateUser(userMario).then(res => {
+    expect(res).toBe("User-1 has been updated.");
+  });
+});
+
+test("delete a user", () => {
+  deleteUser.mockResolvedValue(`User-${userMario.uid} has been deleted.`);
+
+  return deleteUser(userMario).then(res => {
+    expect(res).toBe("User-1 has been deleted.");
+  });
+});
