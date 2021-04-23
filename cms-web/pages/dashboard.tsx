cc639b1970cd92ac64f47ec189743fc7a1e0a7be
seongjinbrian <seongjin2788@gmail.com>
2021-04-23 09:25:35 +0900

    Fix pages

--- cms-web/pages/dashboard.tsx
+++ cms-web/pages/dashboard.tsx
@@ -4,7 +4,7 @@ const Dashboard = ({ id }) => {
   return <div>Login {id}</div>;
 };
 
-// Component.getInitialProps(ctx) 로 전달된 ctx 여기서 contenxt 이다
+
 Dashboard.getInitialProps = async context => {
   return { tag: context.query.tag };
 };
--- cms-web/pages/login.tsx
+++ cms-web/pages/login.tsx
@@ -4,7 +4,7 @@ const Login = ({ id }) => {
   return <div>Login {id}</div>;
 };
 
-// Component.getInitialProps(ctx) 로 전달된 ctx 여기서 contenxt 이다
+
 Login.getInitialProps = async context => {
   return { tag: context.query.tag };
 };
