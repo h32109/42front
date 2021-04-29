d62213d9fefc480a155597b474d8db209292a277
moon <munyohan@naver.com>
2021-04-29 13:12:07 +0900

    Create Dashboard component

---
+++ cms-web/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const dashboard = () => {
+  return <Dashboard>This is facebook dashboard</Dashboard>;
+};
+
+const Dashboard = styled.div`
+  width: 100%;
+  height: 100vh;
+  border: 1px solid red;
+`;
+
+export default dashboard;
